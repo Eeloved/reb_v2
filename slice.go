41d3f99321657fd15c7e904acb4a1e607d8a619f
eeloved <nividim@yandex.ru>
2025-06-17 23:15:42 +0300

    sl

--- slice.go
+++ slice.go
@@ -4,18 +4,21 @@ import "fmt"
 
 func main() {
 	slice := []string{"понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье"}
-
+	//fmt.Println(len(slice), cap(slice))
 	budny := []string{}
 	vihi := []string{}
-
+	toogether := []string{}
 	for _, day := range slice {
 		if day == "суббота" || day == "воскресенье" {
-			budny = append(budny, day)
-		} else {
 			vihi = append(vihi, day)
+		} else {
+			budny = append(budny, day)
 		}
 	}
-	slice = vihi
+	slice = budny
 
-	fmt.Println(budny, vihi)
+	fmt.Println(cap(slice), len(slice), budny, cap(vihi), len(vihi), vihi)
+
+	toogether = append(slice, vihi...)
+	fmt.Println(toogether)
 }
