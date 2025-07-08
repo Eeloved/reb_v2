5a480b7a07703f682d21be2a6966eaad58ebeba4
eeloved <nividim@yandex.ru>
2025-07-08 14:03:28 +0300

    discount

--- m4_01/main.go
+++ m4_01/main.go
@@ -7,7 +7,28 @@ import (
 
 
 func main() {
-	c := iternal.Customer{}
+	cust := iternal.Customer{
+		Age: 23,
+		Balance: 100000,
+		Debt: 1000,
+		Name: "Vasilii",
+	}
+	cust.CalcDiscount = func() (int, error) {
+		if !cust.Discount {
+			return 0, errors.New("Discount not acailable")
+		}
+		result := DEFAULT_DISCOUNT - cust.Debt
+		if result < 0{
+			return 0, nil
+		}
+		return result, nil
+		}
+		discount, _ := cust.CalcDiscount()
 
-	fmt.Printf("%+v\n", c)
-}
+		fmt.Printf("%d", discount)
+	}
+
+	/*cust.Name = "Dima"
+	fmt.Printf(cust.Name)
+
+	//fmt.Printf("%+v\n", cust)*/
