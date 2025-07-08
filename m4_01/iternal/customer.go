04255074a251408a19203175926e4322507b45a2
eeloved <nividim@yandex.ru>
2025-07-08 13:51:19 +0300

    st off

---
+++ m4_01/iternal/customer.go
@@ -0,0 +1,12 @@
+package iternal
+
+type Customer struct {
+	Name   string
+	Age    int
+	Balance int
+	Debt int
+	Discount bool
+	CalcDiscount func() (int, error)
+}
+
+
---
+++ m4_01/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"m4_01/iternal"
+)
+
+
+func main() {
+	c := iternal.Customer{}
+
+	fmt.Printf("%+v\n", c)
+}
