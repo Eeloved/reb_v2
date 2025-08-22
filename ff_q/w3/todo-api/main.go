3869165923a68cea613d463dc75d09307715c0be
eeloved <nividim@yandex.ru>
2025-08-22 12:01:29 +0300

    w3

--- ff_q/w3/todo-api/main.go
+++ ff_q/w3/todo-api/main.go
@@ -17,16 +17,40 @@ var tasks = []Task{
 	{ID: 2, Title: "Whrite API", Done: true},
 }
 
+func createTask(w http.ResponseWriter, r *http.Request) {
+    var newTask Task
+    if err := json.NewDecoder(r.Body).Decode(&newTask); err != nil {
+        http.Error(w, "Неверный JSON", http.StatusBadRequest)
+        return
+    }
+
+    if newTask.Title == "" {
+        http.Error(w, "Поле 'title' обязательно", http.StatusBadRequest)
+        return
+    }
+
+    newTask.ID = len(tasks) + 1
+    tasks = append(tasks, newTask)
+
+    w.Header().Set("Content-Type", "application/json")
+    w.WriteHeader(http.StatusCreated)
+    json.NewEncoder(w).Encode(newTask)
+}
 func main() {
 	http.HandleFunc("/tasks", func(w http.ResponseWriter, r *http.Request){
 		w.Header().Set("Content-Type", "aplication/json")
 
-		if r.Method == "GET" {
-			json.NewEncoder(w).Encode(tasks)
-		} else { 
-			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		}		
+	    switch r.Method {
+    	case "GET":
+        	json.NewEncoder(w).Encode(tasks)//	getTasks(w, r)
+    	case "POST":
+        	createTask(w, r)
+    	default:
+        	http.Error(w, "Метод не поддерживается", http.StatusMethodNotAllowed)
+    }
 	})
+
+	
 
 	log.Println("Server started on http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
