8ed741338cbb91161a921dc4542f99bd0abb9930
eeloved <nividim@yandex.ru>
2025-08-24 17:43:51 +0300

    mux

--- ff_q/w3/todo-api/main.go
+++ ff_q/w3/todo-api/main.go
@@ -4,12 +4,15 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
 type Task struct {
-	ID int `json:"id"`
+	ID    int    `json:"id"`
 	Title string `json:"title"`
-	Done bool `json:"done"`
+	Done  bool   `json:"done"`
 }
 
 var tasks = []Task{
@@ -17,41 +20,108 @@ var tasks = []Task{
 	{ID: 2, Title: "Whrite API", Done: true},
 }
 
-func createTask(w http.ResponseWriter, r *http.Request) {
-    var newTask Task
-    if err := json.NewDecoder(r.Body).Decode(&newTask); err != nil {
-        http.Error(w, "Неверный JSON", http.StatusBadRequest)
-        return
-    }
-
-    if newTask.Title == "" {
-        http.Error(w, "Поле 'title' обязательно", http.StatusBadRequest)
-        return
-    }
-
-    newTask.ID = len(tasks) + 1
-    tasks = append(tasks, newTask)
-
-    w.Header().Set("Content-Type", "application/json")
-    w.WriteHeader(http.StatusCreated)
-    json.NewEncoder(w).Encode(newTask)
+func getTasks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(tasks)
 }
+
+func getTask(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	params := mux.Vars(r)                 // get from url
+	id, err := strconv.Atoi(params["id"]) // convert string to int
+	if err != nil {
+		http.Error(w, "Wrong ID", http.StatusBadRequest)
+		return
+	}
+
+	for _, task := range tasks {
+		if task.ID == id {
+			json.NewEncoder(w).Encode(task)
+			return
+		}
+	}
+	http.Error(w, "Task not found", http.StatusNotFound)
+}
+
+func createTask(w http.ResponseWriter, r *http.Request) {
+	var newTask Task
+	if err := json.NewDecoder(r.Body).Decode(&newTask); err != nil {
+		http.Error(w, "Неверный JSON", http.StatusBadRequest)
+		return
+	}
+
+	if newTask.Title == "" {
+		http.Error(w, "Поле 'title' обязательно", http.StatusBadRequest)
+		return
+	}
+
+	newTask.ID = len(tasks) + 1
+	tasks = append(tasks, newTask)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(newTask)
+}
+
+func updateTask(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	params := mux.Vars(r) // Получаем параметры из URL
+	id, err := strconv.Atoi(params["id"])
+	if err != nil {
+		http.Error(w, "Неверный ID", http.StatusBadRequest)
+		return
+	}
+
+	var updatedTask Task
+	if err := json.NewDecoder(r.Body).Decode(&updatedTask); err != nil {
+		http.Error(w, "Неверный JSON", http.StatusBadRequest)
+		return
+	}
+
+	for i, task := range tasks {
+		if task.ID == id {
+			updatedTask.ID = id
+			tasks[i] = updatedTask
+			json.NewEncoder(w).Encode(updatedTask)
+			return
+		}
+	}
+	http.Error(w, "Task not found", http.StatusNotFound)
+
+}
+func deleteTask(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	params := mux.Vars(r) // Получаем параметры из URL
+	id, err := strconv.Atoi(params["id"])
+	if err != nil {
+		http.Error(w, "Неверный ID", http.StatusBadRequest)
+		return
+	}
+
+	for i, task := range tasks {
+		if task.ID == id {
+			tasks = append(tasks[:i], tasks[i+1:]...) // удаляет элемент по индексу
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+	}
+	http.Error(w, "Task not found", http.StatusNotFound)
+
+}
+
 func main() {
-	http.HandleFunc("/tasks", func(w http.ResponseWriter, r *http.Request){
-		w.Header().Set("Content-Type", "aplication/json")
+	r := mux.NewRouter() // Вместо http.HandleFunc
 
-	    switch r.Method {
-    	case "GET":
-        	json.NewEncoder(w).Encode(tasks)//	getTasks(w, r)
-    	case "POST":
-        	createTask(w, r)
-    	default:
-        	http.Error(w, "Метод не поддерживается", http.StatusMethodNotAllowed)
-    }
-	})
+	// Маршруты
+	r.HandleFunc("/tasks", getTasks).Methods("GET")
+	r.HandleFunc("/tasks", createTask).Methods("POST")
+	r.HandleFunc("/tasks/{id}", getTask).Methods("GET")
+	r.HandleFunc("/tasks/{id}", updateTask).Methods("PUT")
+	r.HandleFunc("/tasks/{id}", deleteTask).Methods("DELETE")
 
-	
-
-	log.Println("Server started on http://localhost:8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Println("Сервер запущен на :8080")
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
